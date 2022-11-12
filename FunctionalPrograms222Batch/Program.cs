acfb61fcb54de7613c473427076d73ec26c76f98
AnilBamne <anilbamne20@gmail.com>
2022-11-12 22:58:13 +0530

    Adding Welcome message

---
+++ FunctionalPrograms222Batch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FunctionalPrograms222Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Functional Programs");
+            
+        }
+    }
+}

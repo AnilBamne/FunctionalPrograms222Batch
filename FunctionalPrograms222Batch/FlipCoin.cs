3deb34c7ff587c1e3e8ed266561702cb2932b51e
AnilBamne <anilbamne20@gmail.com>
2022-11-12 23:34:42 +0530

    Added FlipCoinAndPrintPercentageOfHeadsAndTails Code

---
+++ FunctionalPrograms222Batch/FlipCoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms222Batch
+{
+    public class FlipCoin
+    {
+        public static void FlipTheCoin()
+        {
+            double HeadCount = 0;
+            double TailCount = 0;
+            double HeadPercentage = 0;
+            double TailPercentage = 0;
+
+            Console.WriteLine("How many times you want to flip the coin");
+            int t=Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            for (int i = 1; i <= t; i++)
+            {
+                double option = random.Next(0,2);
+                if (option < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    TailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    HeadCount++;
+                }
+            }
+            HeadPercentage=(HeadCount*100)/t;
+            TailPercentage=(TailCount*100)/t;
+            Console.WriteLine("Percentage of Heads : "+HeadPercentage+"%");
+            Console.WriteLine("Percentage of Tails : "+TailPercentage+"%");
+        }
+    }
+}

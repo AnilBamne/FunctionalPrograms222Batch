985b3b105b5a1d60b03ddeee1bd015db13477457
AnilBamne <anilbamne20@gmail.com>
2022-11-13 16:16:20 +0530

    Adding LeapYear Code

---
+++ FunctionalPrograms222Batch/LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms222Batch
+{
+    public class LeapYear
+    {
+        public void checkLeapYear()
+        {
+            Console.WriteLine("Enter a Year");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if (year == 0)
+            {
+                Console.WriteLine("Please enter a valid year");
+            }
+            else
+            {
+                if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+                {
+                    Console.WriteLine("{0} is a Leap Year", year);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a Leap Year",year);
+                }
+            }
+        } 
+    }
+}
--- FunctionalPrograms222Batch/Program.cs
+++ FunctionalPrograms222Batch/Program.cs
@@ -7,27 +7,25 @@ namespace FunctionalPrograms222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Functional Programs");
-
-            while (true)
+            Console.WriteLine("Select the Program to execute\n1:Flip Coin \n2:Leap year\n3:Power of Two\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-
-                Console.WriteLine("Select the Program to execute\n1:Flip Coin \n2:Leap year\n3:Power of Two\n");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        FlipCoin.FlipTheCoin();
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        break;
-                }
-                Console.WriteLine("**************************");
+                case 1:
+                    FlipCoin.FlipTheCoin();
+                    break;
+                case 2:
+                    LeapYear ly = new LeapYear();
+                    ly.checkLeapYear();
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
             }
-            
+            Console.WriteLine("**************************");
+
+
         }
     }
 }

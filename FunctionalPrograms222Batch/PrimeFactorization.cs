90bc8cc5f4ca0d5b7207c67dc149ea8c6510de6d
AnilBamne <anilbamne20@gmail.com>
2022-11-14 15:55:13 +0530

    Adding PrimeFactors code

---
+++ FunctionalPrograms222Batch/PrimeFactorization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms222Batch
+{
+    internal class PrimeFactorization
+    {
+        public void Factor()
+        {
+            Console.WriteLine("Enter the Number to find Prime Factors");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int factor=1;
+            Console.WriteLine("Prime Factors are :");
+            while (++factor<=n)
+            {
+                if (n % factor == 0)
+                {
+                    Console.WriteLine(factor);
+                    n=n/factor;
+                }
+            }
+            
+        }
+    }
+}
--- FunctionalPrograms222Batch/Program.cs
+++ FunctionalPrograms222Batch/Program.cs
@@ -7,7 +7,7 @@ namespace FunctionalPrograms222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Functional Programs");
-            Console.WriteLine("Select the Program to execute\n\n1:Flip Coin \n2:Leap year\n3:Power of Two\n4:Harmonic Number");
+            Console.WriteLine("Select the Program to execute\n\n1:Flip Coin \n2:Leap year\n3:Power of Two\n4:Harmonic Number\n5:Prime Factorization");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -26,6 +26,10 @@ namespace FunctionalPrograms222Batch
                 case 4:
                     HarmonicNumber harmonicNumber = new HarmonicNumber();
                     harmonicNumber.harmonicNum();
+                    break;
+                case 5:
+                    PrimeFactorization primeFactorization = new PrimeFactorization();
+                    primeFactorization.Factor();
                     break;
                 default:
                     break;

ae3cda10a46eae804c3d398a8dfd3eedda12b1b0
Anil Bamne <99521630+AnilBamne@users.noreply.github.com>
2022-11-16 09:34:49 +0530

    Update ComputeQuotientAndReminder.cs

--- FunctionalPrograms222Batch/ComputeQuotientAndReminder.cs
+++ FunctionalPrograms222Batch/ComputeQuotientAndReminder.cs
@@ -10,12 +10,15 @@ namespace FunctionalPrograms222Batch
     {
         public void computeQR()
         {
+            //Variables
             int Quotiet;
             int Reminder;
+            //Taking user input
             Console.WriteLine("Enter the Devident");
             int divident=int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the Deviser");
             int deviser = int.Parse(Console.ReadLine());
+            //Computation
             Quotiet=divident / deviser;
             Reminder=divident % deviser;
             Console.WriteLine("Quotient = {0} and Reminder = {1}",Quotiet,Reminder);
